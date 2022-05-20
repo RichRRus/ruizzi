835d36db4bc66a60ffc2b2773dded96febace81e
Roman R <R1chRus@yandex.ru>
2022-05-20 22:46:07 +0300

    User models

---
+++ user/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from user import models
+
+
+class PointsInline(admin.TabularInline):
+    model = models.UserPoints
+    can_delete = False
+    max_num = 1
+
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "middle_name", "last_name", "phone", "location")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    inlines = [PointsInline]
---
+++ user/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+
+from user.utils.manager import CustomUserManager
+
+
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+    middle_name = models.CharField(max_length=150, blank=True, verbose_name='Middle name')
+    phone = PhoneNumberField(verbose_name='Phone')
+    location = models.CharField(max_length=511, blank=True, verbose_name='Location')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def get_full_name(self):
+        full_name = f'{self.last_name} {self.first_name} {self.middle_name}'
+        return full_name.strip()
+
+    def __str__(self):
+        return f'{self.get_full_name()} : {self.email}'
+
+
+class UserPoints(models.Model):
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, related_name='points',
+                             verbose_name='Пользователь')
+    amount = models.FloatField(verbose_name='Количество', default=0.0)
+
+    class Meta:
+        verbose_name = 'Баллы пользователя'
+        verbose_name_plural = 'Баллы пользователей'
+        constraints = [
+            models.UniqueConstraint('user', name='unique_user')
+        ]
+
+    def __str__(self):
+        return self.user
