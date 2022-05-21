380e1046795db930f106338958d7ee1bc7b18bce
Roman R <R1chRus@yandex.ru>
2022-05-21 19:10:01 +0300

    User: Password validation

--- user/services.py
+++ user/services.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import UserManager
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import get_default_password_validators
+from django.core.exceptions import ValidationError as DjangoValidationError
 from django.db import transaction
+from rest_framework.exceptions import ValidationError
 
 from user.models import User, UserPoints
 
@@ -8,8 +11,21 @@ from user.models import User, UserPoints
 class UserService:
 
     @staticmethod
+    def __validate_password(password):
+        errors = []
+        password_validators = get_default_password_validators()
+        for validator in password_validators:
+            try:
+                validator.validate(password, None)
+            except DjangoValidationError as error:
+                errors.extend(error.messages)
+        if errors:
+            raise ValidationError(errors)
+
+    @staticmethod
     def create_user(*, email: str, password: str, **extra_fields) -> User:
         email = UserManager.normalize_email(email)
+        UserService.__validate_password(password)
         with transaction.atomic():
             user = User(email=email, username=email, **extra_fields)
             user.password = make_password(password)
