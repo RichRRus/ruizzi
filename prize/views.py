b0bc0cd5c64d9225b7ea33f4efacbb4af05f893e
Roman R <R1chRus@yandex.ru>
2022-05-21 23:01:01 +0300

    Prize: Extended doc schema

--- prize/views.py
+++ prize/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
 
@@ -6,6 +7,28 @@ from prize.serializers import PrizeSerializer
 from user.permissions import IsAdmin
 
 
+@extend_schema_view(
+    list=extend_schema(
+        summary='Список подарков',
+        tags=['prize'],
+    ),
+    retrieve=extend_schema(
+        summary='Информация о подарке',
+        tags=['prize'],
+    ),
+    create=extend_schema(
+        summary='Создание приза',
+        tags=['prize'],
+    ),
+    put=extend_schema(
+        summary='Редактирование приза',
+        tags=['prize'],
+    ),
+    delete=extend_schema(
+        summary='Удаление приза',
+        tags=['prize'],
+    ),
+)
 class PrizeViewSet(viewsets.ModelViewSet):
     queryset = Prize.objects.all()
     serializer_class = PrizeSerializer
